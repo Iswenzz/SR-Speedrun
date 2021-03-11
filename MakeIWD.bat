1f7f394a3d7f76bd970c9753279c80561017ba47
Iswenzz <suxlolz1528@gmail.com>
2021-03-11 19:16:01 +0100

    [Batch] Removed 7za

--- MakeIWD.bat
+++ MakeIWD.bat
@@ -1,30 +1,29 @@
 @ECHO OFF
 
-SET sza="%~dp0\bin\7za.exe"
 SET sz="%~dp0\bin\7z.exe"
 
-echo. & echo [=====] Deleting IWDs [=====] & echo.
+echo. & echo [=====] Deleting IWDs [=====]
 del _images.iwd
 del _sounds.iwd
 del _weapons.iwd
 del _maps_weapons.iwd
 del _z.iwd
 
-echo. & echo [=====] Images [=====] & echo.
-%sza% a -r -tzip _images.iwd images
+echo. & echo [=====] Images [=====]
+%sz% a -r -tzip _images.iwd images
 
-echo. & echo [=====] Sounds [=====] & echo.
-%sza% a -r -tzip _sounds.iwd sound
-%sza% a -r -tzip _sounds.iwd dr_songs.cfg
+echo. & echo [=====] Sounds [=====]
+%sz% a -r -tzip _sounds.iwd sound
+%sz% a -r -tzip _sounds.iwd dr_songs.cfg
 
-echo. & echo [=====] Mod Weapons [=====] & echo.
-%sza% a -r -tzip _weapons.iwd weapons_mod
+echo. & echo [=====] Mod Weapons [=====]
+%sz% a -r -tzip _weapons.iwd weapons_mod
 %sz% rn _weapons.iwd weapons_mod weapons
 
-echo. & echo [=====] Map Weapons [=====] & echo.
-%sza% a -r -tzip _maps_weapons.iwd weapons_maps
+echo. & echo [=====] Map Weapons [=====]
+%sz% a -r -tzip _maps_weapons.iwd weapons_maps
 %sz% rn _maps_weapons.iwd weapons_maps weapons
 
-echo. & echo [=====] Temps [=====] & echo.
-%sza% a -r -tzip _z.iwd _z
-%sz% rn _z.iwd _z sound
+echo. & echo [=====] Temps [=====]
+%sz% a -r -tzip _z.iwd misc/temp_iwds/_z
+%sz% rn _z.iwd misc/temp_iwds/_z sound
--- MakeIWDOnlyWeapons.bat
+++ MakeIWDOnlyWeapons.bat
@@ -1,15 +1,14 @@
 @ECHO OFF
 
-SET sza="%~dp0\bin\7za.exe"
 SET sz="%~dp0\bin\7z.exe"
 
 del _weapons.iwd
 del _maps_weapons.iwd
 
-echo. & echo [=====] Mod Weapons [=====] & echo.
-%sza% a -r -tzip _weapons.iwd weapons_mod
+echo. & echo [=====] Mod Weapons [=====]
+%sz% a -r -tzip _weapons.iwd weapons_mod
 %sz% rn _weapons.iwd weapons_mod weapons
 
-echo. & echo [=====] Map Weapons [=====] & echo.
-%sza% a -r -tzip _maps_weapons.iwd weapons_maps
+echo. & echo [=====] Map Weapons [=====]
+%sz% a -r -tzip _maps_weapons.iwd weapons_maps
 %sz% rn _maps_weapons.iwd weapons_maps weapons
