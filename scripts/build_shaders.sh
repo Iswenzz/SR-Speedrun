ffbdda5867e109c2ea811a72b9f10ee658a3bfba
Iswenzz <suxlolz1528@gmail.com>
2022-08-27 22:02:05 +0200

    Shader: Update

---
+++ scripts/build_shaders.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+source cod4env
+
+# Cleanup
+cd "$MOD"
+rm -v mod.ff
+
+# SR
+cd "$SR/assets"
+cp -rv deffiles "$GAME"
+cp -rv shader_bin "$GAME/raw"
+cp -rv techniques "$GAME/raw"
+cp -rv techsets "$GAME/raw"
+cp -v techsets/* "$GAME/raw/techsets/sm2"
+
+# Assets
+cd "$MOD/assets"
+cp -v mod.csv "$GAME/zone_source"
+cp -rv deffiles "$GAME"
+cp -rv shader_bin "$GAME/raw"
+cp -rv techniques "$GAME/raw"
+cp -rv techsets "$GAME/raw"
+cp -v techsets/* "$GAME/raw/techsets/sm2"
+
+# Shaders
+cd "$GAME/raw/shader_bin"
+./shader_tool sr_shake sr_zoom sr_edge sr_vhs sr_mirror sr_translate sr_blur sr_volumetric_clouds
+
+# Compile
+cd "$GAME/bin"
+./linker_pc -language english -compress -cleanup mod
+cp -v "$GAME/zone/english/mod.ff" "$MOD/mod.ff"
