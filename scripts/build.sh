ae8b81857522b749c88e6e64e82458f0bd383f2a
Iswenzz <suxlolz1528@gmail.com>
2022-09-30 22:24:40 +0200

    Assets: Removed theme

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@ source cod4env
 # Cleanup
 cd "$MOD"
 rm -v mod.ff
+cp -rv maps/mp/gametypes "$GAME/raw/maps/mp"
 
 # SR
 cd "$SR/assets"
@@ -11,7 +12,6 @@ cp -rv animtrees "$GAME/raw"
 cp -rv english "$GAME/raw"
 cp -rv fx "$GAME/raw"
 cp -rv images "$GAME/raw"
-cp -rv maps "$GAME/raw"
 cp -rv material_properties "$GAME/raw"
 cp -rv materials "$GAME/raw"
 cp -rv info "$GAME/raw"
@@ -41,7 +41,6 @@ cp -rv animtrees "$GAME/raw"
 cp -rv english "$GAME/raw"
 cp -rv fx "$GAME/raw"
 cp -rv images "$GAME/raw"
-cp -rv maps "$GAME/raw"
 cp -rv material_properties "$GAME/raw"
 cp -rv materials "$GAME/raw"
 cp -rv info "$GAME/raw"
@@ -67,7 +66,7 @@ cp -rv xmodelsurfs "$GAME/raw"
 
 # Shaders
 cd "$GAME/raw/shader_bin"
-./shader_tool sr_blur_1d17d24e40 sr_edge_0cef709725 sr_glitch_06d9976ae5 sr_psy_glass_11d46ca201 sr_shake_9b04eed583 sr_translate_2fb01cf446 sr_vhs_e7a0beb465 sr_zoom_80ced10fcc sr_psy_edge_b3874b4a78 sr_mirror_52ec30e684 sr_volumetric_clouds_373849a5fc sr_hexagon_83d54f6fd6 sr_aurora_160a9bd1d3 sr_space_curvature_80a4ee4278
+./shader_tool sr_blur_1d17d24e40 sr_edge_0cef709725 sr_glitch_06d9976ae5 sr_psy_glass_11d46ca201 sr_shake_9b04eed583 sr_translate_2fb01cf446 sr_vhs_e7a0beb465 sr_zoom_80ced10fcc sr_psy_edge_b3874b4a78 sr_mirror_52ec30e684 sr_volumetric_clouds_373849a5fc sr_hexagon_83d54f6fd6 sr_aurora_160a9bd1d3 sr_space_curvature_80a4ee4278 sr_grid_dcfcfec4a3
 
 # Compile
 cd "$GAME/bin"
