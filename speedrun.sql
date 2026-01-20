32600029d2c5005b1652cffa221ab8b10c4d6687
Alexis Nardiello <alexisnardiello@gmail.com>
2026-01-20 13:01:04 +0100

    Added db schema

---
+++ speedrun.sql
@@ -0,0 +1,132 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: iswenzz.com    Database: speedrun
+-- ------------------------------------------------------
+-- Server version	8.0.44-0ubuntu0.24.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admins` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(36) COLLATE utf8mb4_general_ci NOT NULL,
+  `player` varchar(36) COLLATE utf8mb4_general_ci NOT NULL,
+  `role` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'player',
+  `vip` int NOT NULL DEFAULT '0',
+  `tas` int NOT NULL DEFAULT '0',
+  `ip` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `date` datetime NOT NULL DEFAULT '2025-01-01 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `admins_UN` (`player`)
+) ENGINE=InnoDB AUTO_INCREMENT=1746 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `bans`
+--
+
+DROP TABLE IF EXISTS `bans`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bans` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(36) COLLATE utf8mb4_general_ci NOT NULL,
+  `guid` varchar(8) COLLATE utf8mb4_general_ci NOT NULL,
+  `player` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `steamId` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `ip` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `leaderboards`
+--
+
+DROP TABLE IF EXISTS `leaderboards`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `leaderboards` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `map` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `player` varchar(36) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(36) COLLATE utf8mb4_general_ci NOT NULL,
+  `mode` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `way` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `time` int NOT NULL,
+  `run` varchar(36) COLLATE utf8mb4_general_ci NOT NULL,
+  `tas` int NOT NULL DEFAULT '0',
+  `date` datetime NOT NULL DEFAULT '2025-01-01 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=113484 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `pbs`
+--
+
+DROP TABLE IF EXISTS `pbs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pbs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `map` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `player` varchar(36) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(36) COLLATE utf8mb4_general_ci NOT NULL,
+  `mode` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `way` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `time` int NOT NULL,
+  `run` varchar(36) COLLATE utf8mb4_general_ci NOT NULL,
+  `date` datetime NOT NULL DEFAULT '2025-01-01 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=237137 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ranks`
+--
+
+DROP TABLE IF EXISTS `ranks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ranks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `player` varchar(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `name` varchar(36) NOT NULL,
+  `xp` int NOT NULL,
+  `level` int NOT NULL,
+  `prestige` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ranks_UN` (`player`)
+) ENGINE=InnoDB AUTO_INCREMENT=27608 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'speedrun'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-01-20 12:56:03
